abf178b59385f981c10715a089d003575583d23b
Mike Bavon <bavomike@gmail.com>
2026-05-08 10:11:37 +0300

    @Resource

--- src/main/java/app/utility/db/DataSourceHelper.java
+++ src/main/java/app/utility/db/DataSourceHelper.java
@@ -1,6 +1,7 @@
 package app.utility.db;
 
 import app.utility.bootstrap.InitBootstrap;
+import jakarta.annotation.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import javax.naming.Context;
@@ -13,25 +14,10 @@ import java.sql.SQLException;
 @ApplicationScoped
 public class DataSourceHelper {
 
-    private static volatile DataSource dataSource;
+    @Resource(lookup = "java:jboss/datasources/Cohort12DS")
+    private DataSource dataSource;
 
     public DataSource getDataSource() {
-        if (dataSource == null) {
-            synchronized (DataSourceHelper.class) {
-                if (dataSource == null) {
-                    try {
-                        //initializing jndi context
-                        Context initCtx = new InitialContext();
-
-                        //look specific datasource
-                        dataSource = (DataSource) initCtx.lookup("java:jboss/datasources/Cohort12DS");
-
-                    } catch (Exception ex){
-                        throw new RuntimeException(ex);
-                    }
-                }
-            }
-        }
         return dataSource;
     }
 
